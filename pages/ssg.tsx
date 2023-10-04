1a73799cb8929ccb61b309302549fe78c502dcbc
CSE485-1851061626-HoangVanSon <63546473+HoangVanSon123456@users.noreply.github.com>
2023-10-04 11:51:18 +0700

    fig ssg

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 
-type Props = {};
+type Props = { person: any[] };
 
-export default function SSG({}: Props) {
-  const person = "Hello from Next.js!";
-  return <div>Hey {person}</div>;
+export async function getStaticProps() {
+  const res = await fetch("https://651cd84f35bd4107e3732c0b.mockapi.io/person");
+  const person = await res.json();
+
+  return {
+    props: {
+      person, // will be passed to the page component as props
+    },
+  };
+}
+
+export default function StaticSiteGeneration({ person }: Props) {
+  return (
+    <>
+      {person.map((e) => (
+        <h2 key={e.id}>{e.person}</h2>
+      ))}
+    </>
+  );
 }
